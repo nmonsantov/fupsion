c073ce5b6d0b7e1952cfea4f06684e477a618b7a
Nelson Monsanto <monsanto_nelson@hotmail.com>
2020-01-10 20:11:35 -0400

    back

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.send('Hello World\n');
+    res.end('Hello World\n');
 }).listen(port);
