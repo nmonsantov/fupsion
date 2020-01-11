24360a5aa7e2d06da2d3e2748a5a31d25c7a12f3
Nelson Monsanto <monsanto_nelson@hotmail.com>
2020-01-11 05:14:02 -0400

    Backup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*jslint node: true */
 "use strict";
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
