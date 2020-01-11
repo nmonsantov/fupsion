073673de82a5320a1377658f47ac124024ca7e3f
Nelson Monsanto <monsanto_nelson@hotmail.com>
2020-01-11 11:25:14 -0400

    Backup

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 const DeviceDetector = require('node-device-detector');
 const detector = new DeviceDetector();
+//----------------------------------------------
+//Para versiones anteriores de Node
+if (!Object.entries)
+   Object.entries = function( obj ){
+      var ownProps = Object.keys( obj ),
+         i = ownProps.length,
+         resArray = new Array(i); // preallocate the Array
 
+      while (i--)
+         resArray[i] = [ownProps[i], obj[ownProps[i]]];
+      return resArray;
+   };
+//---------------------------------------------   
 http.createServer(function (req, res) {
     //Verificar el dispositivo del cliente
     let device=viewstart(req.headers["user-agent"]);
-    
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n'+device);
 }).listen(port);
