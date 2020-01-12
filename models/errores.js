09c4c91c20d4b37b78925f0d782ab9a05ee76c00
Nelson Monsanto <monsanto_nelson@hotmail.com>
2020-01-12 10:32:04 -0400

    Bk202012011

---
+++ models/errores.js
@@ -0,0 +1,12 @@
+function Errores(){
+    this.error_number =0;
+    this.error_message="";
+    this.error_code="";
+    this.html=null;
+    this.params=null;
+    this.registros=0;
+    this.pathfile=null;
+    this.data=null;
+}
+
+module.exports = Errores;
